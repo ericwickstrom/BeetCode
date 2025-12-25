a7491764dcbe4d90ea1c63eb78aa35860c53f040
Eric Wickstrom <wickstrome@gmail.com>
2025-12-25 12:24:42 -0700

    704 done

--- Problems/Problem704.cs
+++ Problems/Problem704.cs
@@ -1,5 +1,6 @@
 using BeetCode.Framework;
 using System.Collections.Generic;
+using System.Globalization;
 
 namespace BeetCode.Problems
 {
@@ -87,7 +88,20 @@ namespace BeetCode.Problems
         // YOUR SOLUTION GOES HERE
         public int Search(int[] nums, int target)
         {
-            throw new System.NotImplementedException();
+            int left = 0;
+            int right = nums.Length - 1;
+
+            while(left <= right)
+            {
+                int mid = left + (right - left) / 2;
+                
+                if(nums[mid] == target) return mid;
+
+                if(nums[mid] < target) left = mid + 1;
+                else right = mid - 1;
+            }
+
+            return -1;
         }
     }
 }
